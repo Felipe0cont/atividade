90ee117564a7da27707ddbc67d56d14f224d29d7
Felipe0cont <felipe.teles.duarte@escola.pr.gov.br>
2025-03-12 12:29:58 -0300

    Add files via upload

---
+++ atividade pc 3.c
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+
+int main(){
+    int ano;
+    scanf("%d", & ano);
+    
+    if (((ano % 4 == 0)&& !(ano % 100 == 0))||(ano % 400 == 0)){
+        
+        printf("bissexto");
+        
+    }
+        else{ 
+        printf("não foi bussexto");
+        
+    }
+    
+    return 0;
+}
